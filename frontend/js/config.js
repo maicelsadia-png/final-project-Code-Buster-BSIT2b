aa6afe656a53ad2a12b2a52a4512301a12526d51
AJ011 <aedanpielago2005@gmail.com>
2026-05-15 12:10:33 +0800

    Update BACKEND_URL to production URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,6 +3,6 @@
  * Change this ONE line if the backend port ever changes.
  * All other JS files reference window.API_BASE or window.BACKEND_URL.
  */
-var BACKEND_URL  = 'http://localhost:3000';
+var BACKEND_URL  = 'https://crumbelle.onrender.com';
 var API_BASE_URL = BACKEND_URL + '/api';
 var API_BASE     = API_BASE_URL;
